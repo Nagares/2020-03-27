52ed038c6e02cbf5cf46a2e36a93f36f4aa21d6e
Anya <youremail@domain.com>
2020-03-29 13:28:11 +0300

    add home work

--- js/index.js
+++ js/index.js
@@ -5,10 +5,7 @@ const $updateStudent = $('#update-student');
 const $deleteStudent = $('#delete-student');
 
 const db = new DataBase('https://frontend-lectures.firebaseio.com', 1);
-
-/*db.deleteStudent('03ilugmu8q').then(response => {
-	console.log('response', response);
-});*/
+clear()
 
 db.getStudents().then(response => {
 	const students = Object.entries(response).map(item => {
@@ -18,6 +15,7 @@ db.getStudents().then(response => {
 	});
 
 	students.forEach(student => {
+
 		$('<a>')
 			.text(`${student.firstname} ${student.lastname}`)
 			.addClass('list-group-item')
@@ -31,8 +29,9 @@ db.getStudents().then(response => {
 
 $studentsList.on('click', '[data-id]', function(event) {
 	event.preventDefault();
-
 	const studentId = $(this).data('id');
+
+ 	$deleteStudent.attr('dell-id', studentId);
 
 	db.getStudent(studentId).then(response => {
 		for (let key in response) {
@@ -43,7 +42,7 @@ $studentsList.on('click', '[data-id]', function(event) {
 
 $updateStudent.on('submit', function(event) {
 	event.preventDefault();
-
+    
 	const elements = Array.from(this.elements);
 
 	const data = {};
@@ -59,3 +58,33 @@ $updateStudent.on('submit', function(event) {
 
 	console.log(data);
 });
+
+
+$deleteStudent.on('click', function(event) {
+
+	let studentDellId = $(this).attr('dell-id');
+		
+	let findStudent = Array.from($studentsList.children())
+
+	let theStudent = findStudent.find((item) => {
+			
+			return $(item).attr('data-id') === studentDellId;
+		
+		});
+	
+	$(theStudent).remove()
+	
+
+	db.deleteStudent(studentDellId).then(response => {
+		clear()
+	});
+});
+
+
+function clear(){
+
+		for (let i=0; i < $updateStudent[0].length; i++) {
+
+					$updateStudent.find(`input`).val(``);
+		}
+};
